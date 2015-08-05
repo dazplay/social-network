01f0ec9d6e6f40aa21d95dc8bf092759d46beea1
Daz <dazmcg@gmail.com>
2015-08-05 08:42:47 +0100

    Make repositories return immutable lists when queried.

--- src/main/java/uk/me/imprison/social/InMemoryMessagesStore.java
+++ src/main/java/uk/me/imprison/social/InMemoryMessagesStore.java
@@ -1,27 +1,36 @@
 package uk.me.imprison.social;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collector;
 
 import com.google.common.collect.Lists;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class InMemoryMessagesStore implements MessagesStore {
-    List<Message> messages = Lists.newArrayList();
+    private final List<Message> messages = newArrayList();
 
     @Override public void add(Message message) {
         messages.add(message);
     }
 
     @Override public List<Message> getMessagesBelongingTo(UserName user) {
-        return messages.stream().filter(by(user)).collect(toList());
+        return getMessagesBelongingTo(singletonList(user));
     }
 
     @Override public List<Message> getMessagesBelongingTo(Collection<UserName> users) {
-        return messages.stream().filter(by(users)).collect(toList());
+        return messages.stream().filter(by(users)).collect(toUnmodifiableList());
+    }
+
+    private Collector<Message, ?, List<Message>> toUnmodifiableList() {
+        return collectingAndThen(toList(), Collections::unmodifiableList);
     }
 
     private Predicate<? super Message> by(Collection<UserName> users) {
--- src/main/java/uk/me/imprison/social/InMemorySocialNetwork.java
+++ src/main/java/uk/me/imprison/social/InMemorySocialNetwork.java
@@ -3,13 +3,14 @@ package uk.me.imprison.social;
 import java.util.Collection;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.SetMultimap;
 
 public class InMemorySocialNetwork implements SocialNetwork {
     private final SetMultimap<UserName, UserName> followees = HashMultimap.create();
 
     @Override public Collection<UserName> followeesFor(final UserName userName) {
-        return followees.get(userName);
+        return ImmutableList.copyOf(followees.get(userName));
     }
 
     @Override public void follows(final UserName follower, final UserName followee) {
